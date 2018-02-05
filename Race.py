1307489086072ff345f75f131a0c129bde0127cc
Lope Ramos <loperamosmunoz@gmail.com>
2018-02-05 08:53:44 +0100

    Added gitignore

--- Race.py
+++ Race.py
@@ -20,7 +20,13 @@ k_shield = 'SHIELD'
 k_type = 'type'
 k_thrust = 'thrust'
 
-def rotate(origin:np.array, point:np.array, angle):
+e1 = 'e1'
+e2 = 'e2'
+p1 = 'p1'
+p2 = 'p2'
+
+
+def rotate(origin: np.array, point: np.array, angle):
     """
     Rotate a point counterclockwise by a given angle around a given origin.
 
@@ -40,16 +46,16 @@ def a(v0, thrust=0):
 
 
 def v_t(v0, t, thrust=0):
-    a = ka * thrust
-    b = 0.21
-    return a + (v0 + a)*math.exp(-b*t)
+    _a = ka * thrust
+    _b = 0.21
+    return _a + (v0 + _a)*math.exp(-_b*t)
 
 
 def e_t(v0, t, e0=0, thrust=0):
-    a = ka*thrust
-    b = v0 - ka*thrust
-    c = 0.21
-    return e0 + b/c + a*t - b/c*math.exp(-c*t)
+    _a = ka*thrust
+    _b = v0 - ka*thrust
+    _c = 0.21
+    return e0 + _b/_c + _a*t - _b/_c*math.exp(-_c*t)
 
 
 def t_e(e, v0, thrust=0):
@@ -71,7 +77,7 @@ def t_e(e, v0, thrust=0):
     return t
 
 
-def ai(v_i, thrust = 0):
+def ai(v_i, thrust=0):
     return 0.85*thrust - 0.21*v_i
 
 
@@ -83,9 +89,22 @@ def eii(e_i, v_i):
     return e_i + v_i*dt
 
 
+def ai2d(v_i: np.array, thrust: np.array=np.array([0, 0])):
+    return 0.85**thrust - 0.21**v_i
+
+
+def vii2d(v_i: np.array, a_i: np.array):
+    return v_i + a_i ** dt
+
+
+def eii2d(e_i: np.array, v_i: np.array):
+    return e_i + v_i**dt
+
+
 def u_vec(vector):
     """ Returns the unit vector of the vector.  """
-    if norm(vector) == 0: return np.array([0,0])
+    if norm(vector) == 0:
+        return np.array([0, 0])
     return vector / norm(vector)
 
 
@@ -103,8 +122,8 @@ def v_ang(v):
     return np.arctan2(v_u[1], v_u[0])
 
 
-def ang_set(ang):
-    return ang % 2*math.pi
+def ang_set(i_ang):
+    return i_ang % 2*math.pi
 
 
 class Check:
@@ -123,7 +142,7 @@ class Check:
 
 class Pod:
     def __init__(self, name, i_x=-1, i_y=-1):
-        self.name=name
+        self.name = name
         self.p = np.array([i_x, i_y])
         self.p_prev = np.array([i_x, i_y])
         self.v = np.array([0, 0])
@@ -137,6 +156,12 @@ class Pod:
 
         self.turning_started = False
         self.boost_used = False
+
+    def __eq__(self, other):
+        return self.name == other
+
+    def __ne__(self, other):
+        return self.name != other
 
     def add_step(self, i_p: np.array, i_v: np.array, i_a: int):
         print("Pod " + str(self.name) + ' i_p: ' + str(i_p) + ' i_v: ' + str(i_v), file=sys.stderr)
@@ -161,7 +186,7 @@ class Pod:
     #     print("pod orientation angl: " + str(self.a * 180 / math.pi), file=sys.stderr)
 
     def collision(self):
-        #print('p: ' + str(self.p) + ' p_prev: ' + str(self.p_prev), file=sys.stderr)
+        # print('p: ' + str(self.p) + ' p_prev: ' + str(self.p_prev), file=sys.stderr)
         collision = abs(ang(self.v, self.p - self.p_prev) * deg) > 2
         if collision:
             print("collision detected on pod " + str(self.name), file=sys.stderr)
@@ -173,7 +198,7 @@ class Pod:
         if self.check_id != next_check_id:
             self.turning_started = False
             self.check_id = next_check_id
-        self.add_step(np.array([x,y]), np.array([vx, vy]), angle/deg)
+        self.add_step(np.array([x, y]), np.array([vx, vy]), angle/deg)
         if self.collision():
             self.turning_started = False
 
@@ -181,12 +206,18 @@ class Pod:
         if self.out_params[k_type] == k_boost:
             print("BOOSTING", file=sys.stderr)
             self.boost_used = True
-            print(str(int(self.out_params[k_pos][0])) + " " + str(int(self.out_params[k_pos][1])) + " " + k_boost+ " " + k_boost)
+            print(str(int(self.out_params[k_pos][0])) + " "
+                  + str(int(self.out_params[k_pos][1]))
+                  + " " + k_boost + " " + k_boost)
         elif self.out_params[k_type] == k_shield:
             print("SHIELDING", file=sys.stderr)
-            print(str(int(self.out_params[k_pos][0])) + " " + str(int(self.out_params[k_pos][1])) + " " + k_shield+ " " + k_shield)
+            print(str(int(self.out_params[k_pos][0]))
+                  + " " + str(int(self.out_params[k_pos][1]))
+                  + " " + k_shield + " " + k_shield)
         else:
-            print(str(int(self.out_params[k_pos][0])) + " " + str(int(self.out_params[k_pos][1])) + " " + str(int(self.out_params[k_thrust])))
+            print(str(int(self.out_params[k_pos][0]))
+                  + " " + str(int(self.out_params[k_pos][1]))
+                  + " " + str(int(self.out_params[k_thrust])))
 
 
 class Map:
@@ -199,8 +230,8 @@ class Map:
         self.checks.append(i_check)
 
     def get_next_check(self, pos, dif_pos=1):
-        next_pos = (pos + dif_pos)%self.num_of_checks
-        return {k_pos:next_pos,k_check:copy(self.checks[next_pos])}
+        next_pos = (pos + dif_pos) % self.num_of_checks
+        return {k_pos: next_pos, k_check: copy(self.checks[next_pos])}
 
     def get_next_check_p(self, pos, dif_pos=1):
         return self.get_next_check(pos, dif_pos)[k_check].p
@@ -213,28 +244,28 @@ class Map:
 
 
 def iterate_until_dist(dist, v0):
-    max = 100
-    a = np.zeros(max)
-    v = np.zeros(max)
-    e = np.zeros(max)
-    a[0]=ai(v0)
-    v[0]=v0
+    max_t = 100
+    accel = np.zeros(max_t)
+    v = np.zeros(max_t)
+    e = np.zeros(max_t)
+    accel[0] = ai(v0)
+    v[0] = v0
     t = 0
-    while e[t] < dist and t+1 < max:
-        a[t] = ai(v[t])
-        v[t+1] = vii(v[t], a[t])
+    while e[t] < dist and t+1 < max_t:
+        accel[t] = ai(v[t])
+        v[t+1] = vii(v[t], accel[t])
         e[t+1] = eii(e[t], v[t])
         t += 1
-    return {"t": t, 'a':a[t], 'v':v[t], 'e':e[t]}
+    return {"t": t, 'a': accel[t], 'v': v[t], 'e': e[t]}
 
 
 class Game:
     def __init__(self):
         self.map = Map()
-        self.enemy_pod_1 = Pod('e1')
-        self.enemy_pod_2 = Pod('e2')
-        self.player_pod_1 = Pod('p1')
-        self.player_pod_2 = Pod('p2')
+        self.pods = {e1: Pod(e1),
+                     e2: Pod(e2),
+                     p1: Pod(p1),
+                     p2: Pod(p2)}
         self.step = 0
         self.slow_down = False
         self.send_boost = False
@@ -247,46 +278,51 @@ class Game:
         check_count = int(input())
         self.map = Map(laps, check_count)
         for check_pos in range(check_count):
-            check_x , check_y = [int(i) for i in input().split()]
+            check_x, check_y = [int(i) for i in input().split()]
             self.map.add_check(Check(check_x, check_y))
 
     def receive_input(self):
-        self.player_pod_1.update_status()
-        self.player_pod_2.update_status()
-        self.enemy_pod_1.update_status()
-        self.enemy_pod_2.update_status()
+        self.pods[p1].update_status()
+        self.pods[p2].update_status()
+        self.pods[e1].update_status()
+        self.pods[e2].update_status()
 
     def send_output(self):
-        self.player_pod_1.send_output()
-        self.player_pod_2.send_output()
-        self.step +=1
+        self.pods[p1].send_output()
+        self.pods[p2].send_output()
+        self.step += 1
 
-    def calculate_point_speed(self, pod:Pod):
+    def collision_incoming(self, pod: Pod):
+        for inc_pod in self.pods.values():
+            if inc_pod == pod:
+                continue
+            pos_a = eii(inc_pod.p, inc_pod.v)
 
+    def calculate_point_speed(self, pod: Pod):
         next_current_angle = v_ang(self.map.get_next_check_p(pod.check_id) - self.map.get_check_p(pod.check_id))
-        speed_on_point = v_max - (v_max - v_min)/math.pi * abs(next_current_angle - pod.a)
-        if speed_on_point < v_min:
-            speed_on_point = v_min
+        speed_on_next_point = v_max - (v_max - v_min)/math.pi * abs(next_current_angle - pod.a)
+        if speed_on_next_point < v_min:
+            speed_on_next_point = v_min
 
         # print(str(speed_on_point) + ' = '
         #       + str(v_max) + ' - ( ' + str(v_max) + ' - ' + str(v_min) + ')/math.pi * ' + str(
         #     abs(next_current_angle - pod.a)), file=sys.stderr)
 
-        #print("speed_on_point: " + str(speed_on_point), file=sys.stderr)
-        return speed_on_point
+        # print("speed_on_point: " + str(speed_on_point), file=sys.stderr)
+        return speed_on_next_point
 
     def calculate_thrusts(self):
-        self.calculate_thrust(self.player_pod_1)
-        self.calculate_thrust(self.player_pod_2)
+        self.calculate_thrust(self.pods[p1])
+        self.calculate_thrust(self.pods[p2])
 
     def calculate_dests(self):
-        self.calculate_dest(self.player_pod_1)
-        self.calculate_dest(self.player_pod_2)
+        self.calculate_dest(self.pods[p1])
+        self.calculate_dest(self.pods[p2])
 
-    def calculate_thrust(self, pod:Pod):
+    def calculate_thrust(self, pod: Pod):
         # print("Calculating thrust", file=sys.stderr)
         self.slow_down = self.should_i_slowdown(self.calculate_point_speed(pod), pod)
-        #print("Angle diff: " + str(self.player_pod.a_to_check * 180 / math.pi), file=sys.stderr)
+        # print("Angle diff: " + str(self.player_pod.a_to_check * 180 / math.pi), file=sys.stderr)
         if self.slow_down or \
                 (abs(ang(self.map.get_check_p(pod.check_id)-pod.p, pod.v)) * deg > 80
                  and norm(pod.v) > v_min):
@@ -299,44 +335,44 @@ class Game:
         if not pod.boost_used \
                 and pod.out_params[k_thrust] == 100 \
                 and abs(ang(self.map.get_check_p(pod.check_id)-pod.p, pod.v)) * deg < 10 \
-                and abs(pod.a - v_ang(pod.v))* deg < 10 \
-                and norm(self.map.get_check_p(pod.check_id)- pod.p) > 5000:
+                and abs(pod.a - v_ang(pod.v)) * deg < 10 \
+                and norm(self.map.get_check_p(pod.check_id) - pod.p) > 5000:
             pod.out_params[k_type] = k_boost
 
-
-    def get_speed_check_ang(self, to_point, pod:Pod):
+    @staticmethod
+    def get_speed_check_ang(to_point, pod: Pod):
         # print("check: " + str(self.map.get_current_check().p) +
         #       "- point: " + str(pod.p) +
         #       " speed: " + str(pod.v), file=sys.stderr)
         return ang(to_point - pod.p, pod.v)
 
-
-    def calc_ang(self, pod:Pod):
-        aNC = ang_set(v_ang(self.map.get_check_p(pod.check_id+1) - self.map.get_check_p(pod.check_id)))
-        aP = ang_set(pod.a)
-        if aNC > aP:
-            med = ang_set((aNC - aP)/2)
-            beta = aP - med
+    def calc_ang(self, pod: Pod):
+        a_nc = ang_set(v_ang(self.map.get_check_p(pod.check_id+1) - self.map.get_check_p(pod.check_id)))
+        a_p = ang_set(pod.a)
+        if a_nc > a_p:
+            med = ang_set((a_nc - a_p)/2)
+            beta = a_p - med
         else:
-            med = ang_set((aP - aNC)/2)
-            beta = aNC - med
+            med = ang_set((a_p - a_nc)/2)
+            beta = a_nc - med
 
-        print ('aNC:' + str(aNC*deg) + ' aP: ' + str(aP*deg) + ' med: ' + str(med*deg) + ' beta = ' + str(beta*deg), file=sys.stderr)
+        print('aNC:' + str(a_nc*deg) + ' a_p: ' + str(a_p*deg) + ' med: ' + str(med*deg) + ' beta = ' + str(beta*deg),
+              file=sys.stderr)
         return ang_set(beta)
 
-
-    def correct_dest(self, pod:Pod):
+    def correct_dest(self, pod: Pod):
         pointing_to = copy(self.map.get_check_p(pod.check_id))
         beta = self.calc_ang(pod)
         print("beta: " + str(beta), file=sys.stderr)
-        pointing_to[0]+=600*math.cos(beta)
-        pointing_to[1]+=600*math.sin(beta)
+        pointing_to[0] += 600*math.cos(beta)
+        pointing_to[1] += 600*math.sin(beta)
         return pointing_to
 
-    def calculate_dest(self, pod:Pod):
+    def calculate_dest(self, pod: Pod):
         # print("angle to check: " + str(self.get_speed_check_ang() * 180 / math.pi), file=sys.stderr)
         pointing_to = self.correct_dest(pod)
-        print(str(pod.name) + ' check_id: ' + str(pod.check_id) + '->'+ str(self.map.get_check_p(pod.check_id)), file=sys.stderr)
+        print(str(pod.name) + ' check_id: ' + str(pod.check_id) + '->'
+              + str(self.map.get_check_p(pod.check_id)), file=sys.stderr)
         # pointing_to = self.map.get_check_p(pod.check_id)
         if abs(self.get_speed_check_ang(pointing_to, pod) * deg) < 85:
             pod.out_params[k_pos] = rotate(pod.p, pointing_to, - self.get_speed_check_ang(pointing_to, pod))
@@ -345,18 +381,18 @@ class Game:
         if self.slow_down:
             pod.out_params[k_pos] = self.map.get_check_p(pod.check_id + 1)
 
-    def should_i_slowdown(self, v_d, pod:Pod):
+    def should_i_slowdown(self, v_d, pod: Pod):
         dist = np.linalg.norm(pod.p - self.map.get_check_p(pod.check_id))
         v_proj = np.dot(pod.v, u_vec(self.map.get_check_p(pod.check_id)-pod.p))
-        #print("dist: " + str(dist), file=sys.stderr)
-        #print("v_proj: " + str(v_proj), file=sys.stderr)
-        #t = t_e(dist - 600, v_proj)
+        # print("dist: " + str(dist), file=sys.stderr)
+        # print("v_proj: " + str(v_proj), file=sys.stderr)
+        # t = t_e(dist - 600, v_proj)
         info = iterate_until_dist(dist - 600, v_proj)
         # info = {'t':0, 'a':0, 'e':0, 'v':0, }
-        #print("t_point: " + str(info['t']), file=sys.stderr)
-        #print("a_point: " + str(info['a']), file=sys.stderr)
-        #print("e_point: " + str(info['e']), file=sys.stderr)
-        #print("v_point: " + str(info['v']), file=sys.stderr)
+        # print("t_point: " + str(info['t']), file=sys.stderr)
+        # print("a_point: " + str(info['a']), file=sys.stderr)
+        # print("e_point: " + str(info['e']), file=sys.stderr)
+        # print("v_point: " + str(info['v']), file=sys.stderr)
         if pod.turning_started:
             return True
         elif info['v'] > v_d:
